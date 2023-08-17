bd4be9790e163c4636ea846de6ed73e41b3364f0
Hamza <hamzasajid4040@gmail.com>
2023-08-17 15:20:51 +0500

    added handshake function

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,41 @@ const ETHEREUM_MAGIC: [u8; 4] = [0x22, 0x80, 0x9D, 0xF2];
 const ETHEREUM_VERSION: u32 = 5;
 
 //handshake function 
-fn perform_ethereum_handshake() {
+fn perform_ethereum_handshake<A: ToSocketAddrs>(addr: A) -> Result<(), Box<dyn std::error::Error>> {
+    let mut stream = TcpStream::connect(addr)?;
 
+    // Send the Ethereum handshake
+    let mut handshake = Vec::new();
+    handshake.extend_from_slice(&ETHEREUM_MAGIC);
+    handshake.extend_from_slice(&ETHEREUM_VERSION.to_be_bytes());
+
+    // Send the handshake payload
+    stream.write_all(&handshake)?;
+
+    // Read and process the response
+    let mut response_magic = [0u8; 4];
+    let mut response_version = [0u8; 4];
+
+    stream.read_exact(&mut response_magic)?;
+    stream.read_exact(&mut response_version)?;
+
+    if response_magic != ETHEREUM_MAGIC {
+        return Err("Invalid magic bytes in response".into());
+    }
+
+    let remote_version = u32::from_be_bytes(response_version);
+
+    // ... (Process the response, perform additional verification if needed)
+
+    println!("Ethereum handshake successful. Remote version: {}", remote_version);
+
+    Ok(())
 }
+
 
 //main function
 fn main() {
    //here handshake function will be called
+   
    
 }
