65795fcde71fe3176d57bf12da250b93eeb57a07
FurqanAzizDev <furqan.dev11@invomail.io>
2023-08-17 10:43:22 +0500

    tcp handshake base code

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+
+use std::io::{Read, Write};
+use std::net::{TcpStream, ToSocketAddrs};
+
+// Ethereum protocol magic bytes
+const ETHEREUM_MAGIC: [u8; 4] = [0x22, 0x80, 0x9D, 0xF2];
+// Ethereum protocol version
+const ETHEREUM_VERSION: u32 = 5;
+
+//handshake function 
+fn perform_ethereum_handshake() {
+
+}
+
+//main function
+fn main() {
+   //here handshake function will be called
+   
+}
